cd0affd58c25a986bdfbbc4386c2ba0639b75f33
gcb906 <grenaajacob@icloud.com>
2023-03-19 16:54:00 +0100

    Home production

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -43,9 +43,6 @@ class householdClass:
         C = par.wageM*LM + par.wageF*LF
         
         # Home production
-        if par.sigma == 1:
-            H = HM**(1-par.alpha)*HF**par.alpha
-        else:
             H = ((1-par.alpha)*HM**(par.sigma-1/par.sigma)+par.alpha*HF**(par.sigma-1/par.sigma))**(par.sigma/par.sigma-1)
 
         # Total consumption
