90721f2eb450f84d0872c3907ce17890f6445932
gcb906 <grenaajacob@icloud.com>
2023-03-19 14:29:20 +0100

    Definerer variable

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,15 +1,16 @@
-def square(x):
-    """ square numpy array
-    
-    Args:
-    
-        x (ndarray): input array
-        
-    Returns:
-    
-        y (ndarray): output array
-    
-    """
-    
-    y = x**2
-    return y
+from types import SimpleNamespace
+
+class householdClass:
+
+    def __init__(self):
+
+        par = self.par = SimpleNamespace()
+
+        par.rho = 2
+        par.nu = 0.001
+        par.epsilon = 1
+        par.omega = 0.5
+        par.alpha = 0.5
+        par.sigma = 1
+        par.wageF = 1
+        par.wageM = 1
