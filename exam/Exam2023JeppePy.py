f93de2f81487d0a41337b76459c001cb2449992f
EmilErbas <jeppeharbo@live.dk>
2023-05-25 17:14:52 +0200

    Hala

--- exam/Exam2023JeppePy.py
+++ exam/Exam2023JeppePy.py
@@ -14,33 +14,36 @@ class ModelQ2:
         par = self.par = SimpleNamespace()
         opt = self.opt = SimpleNamespace()
         
-
         # Baseline parameters 
         par.eta = .5 
         par.wage = 1   
-        par.kappa = [1.0,2.0]
+        par.kappa = np.linspace(1,2,2)
         par.rho = .9
         par.iota = .01
         par.sigma = 0.1 
         par.rate = (1+0.01)**(1/12)
         
-
         #Dynamic model parameters
         par.time = 120
         par.l0 = 0
-        par.kappaprev = 0
+        par.kappaprev = 1
+        par.K = 1000
+        par.delta = 0.05
         
         # Defining variables
         par.l = (((1-par.eta)*par.kappa)/par.wage)**(1/par.eta)
         par.y = par.l
         par.p = par.kappa*par.y**(-par.eta)
+        par.l_star = ((1-par.eta) * par.kappa / par.wage)**(1/par.eta)
 
-        #Storaging results
-        opt.epsilon = np.zeros(par.time)
-        opt.ex_postval
+        #Storages to overwrite later and starting vals
+        opt.epsilon = np.zeros(len(par.time))
+        opt.ex_postval = 0
+        opt.ex_postval_pol = 0
 
         #Vectors for storage 
-        opt.ex_postvals =[] 
+        opt.ex_postvals = [] 
+        opt.ex_postvals_pol = []
 
     def calculate_profit(self):
 
@@ -83,20 +86,45 @@ class ModelQ2:
             par.l0 = par.l
             par.kappaprev = par.kappa
     
-        for i in range(K):
+        for t in range(par.K):
             opt.epsilon = self.AR_shocks()
             opt.ex_postval = self.calc_ex_post()
             opt.ex_postvals.append(opt.ex_postval)
 
         H = np.mean(opt.ex_postvals)
-        print = print(f"The approximate ex ante expected value of the salon (H) is: {H}")
+        print_result = print(f"The approximate ex ante expected value of the salon (H) is: {H}")
         
-        return print
+        return print_result
+
+    def adjustl(self): 
+        #Calls on the params 
+        par = self.par 
+        opt = self.opt
+        if abs(par.l0 - par.l_star) > par.delta:
+            return par.l_star
+        else: 
+            return par.l0
+
+    def calc_ex_post_policy(self):
+        #Calls on the params 
+        par = self.par 
+        opt = self.opt
+
+        for t in range(par.time):
+            par.kappa = par.rho * np.log(par.kappaprev) + [t]
+            par.kappa = np.exp(par.kappa)
+            par.l = self.adjustl()
+            Profit = par.kappa * par.l ** (1 - par.eta) - par.wage * par.l - (1 if par.l != par.l0 else 0) * par.iota
+            opt.ex_postval_pol += par.R ** (-t) * Profit
+            par.l0 = par.l
+            par.kappaprev = par.kappa
+
+
+        for t in range(par.K):
+            opt.epsilon = self.AR_shocks()
+            opt.ex_postval_pol = self.calc_ex_post_policy
+            opt.ex_postval
     
-    
-
-
-
     
 
 
