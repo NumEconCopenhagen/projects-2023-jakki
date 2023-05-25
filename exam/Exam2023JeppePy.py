657ec629c7c2dc9c097f304e6844cb289a2edc76
jhvander <jeppeharbo@live.dk>
2023-05-25 12:27:41 +0200

    commit

--- exam/Exam2023JeppePy.py
+++ exam/Exam2023JeppePy.py
@@ -10,11 +10,12 @@ class ModelQ2:
     def __init__(self):
         """ setup model """
 
-        # a. create namespaces
+        # Create namespaces
         par = self.par = SimpleNamespace()
         opt = self.opt = SimpleNamespace()
+        
 
-        # b. baseline parameters 
+        # Baseline parameters 
         par.eta = .5 
         par.wage = 1   
         par.kappa = np.linspace(1,2,2)
@@ -23,7 +24,66 @@ class ModelQ2:
         par.sigma = 0.1 
         par.rate = (1+0.01)**(1/12)
 
-        # c.  
+        #Dynamic model parameters
+        par.time = 120
+        par.l0 = 0
+
+        # Defining variables
+        par.l = (((1-par.eta)*par.kappa)/par.wage)**(1/par.eta)
+        par.y = par.l
+        par.p = par.kappa*par.y**(-par.eta)
+
+        #Vectors to store results
+        opt.logkappa = np.zeros(par.time)
+        opt.H_val =np.zeros(par.time)    
+
+    def function1(self):
+
+        #Calls on the Params
+        par = self.par
+        opt = self.opt
+        #Sets up the function
+        Pi = par.kappa*par.l**(1-par.eta)-par.wage*par.l
+        #Finding max value for Pi and l
+        Profit = np.argmax(Pi) #Finds the max value of Pi given inputs 
+        Profit_l = par.l[Profit] #Finding the subsequential highest value of l based on Profit
+        Profit_val = Pi[Profit] #Finding the highest value of Pi. Max profit
+
+        #Printing my findings 
+        result1 = f'The maximum profit is {Profit_val}'
+        result2 = f'This happens when l is {Profit_l}'
+        result3 = f'When kappa is {par.kappa[Profit]}'
+        #Make it return our prints
+        return result1, result2, result3 
+    
+    def AR_shocks(self):
+        #Calls on the params
+        par = self.par
+        opt = self.opt 
+
+        for i in range(1,par.time):
+            par.kappa[i] = par.rho*np.log(par.kappa[i-1])+np.random.normal(-.5*par.sigma**2,par.sigma, par.time) 
+            opt.logkappa = np.log(par.kappa[i])
+
+        return opt.logkappa
+    
+    def functionH(self):
+        #Calls on the params
+        par = self.par
+        opt = self.opt
+
+        for t in range(1, par.time):
+            par.l[t] = ((1-par.eta)*par.kappa[t]/par.wage)**(1/par.eta)
+            opt.H_val[t] = par.rate **(-t) * (par.kappa[t] * par.l[t]**(1-par.eta)-par.wage*par.l[t]-int(par.l[t]!= par.l[t-1])*par.iota)
+
+            #Then taking the mean 
+    
+
+
+
+    
+
+
 
 
 
